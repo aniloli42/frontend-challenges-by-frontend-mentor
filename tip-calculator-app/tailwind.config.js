624489ee2b08ffe4bb939925db03f8ceb08caae5
aniloli42 <aniloli42@gmail.com>
2022-07-05 17:16:39 +0545

    chore: file structure building

--- tip-calculator-app/tailwind.config.js
+++ tip-calculator-app/tailwind.config.js
@@ -1,7 +1,17 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{js,jsx,ts,tsx}", "./component/**/*.{js,jsx,ts,tsx}"],
+  content: ["pages/**/*.{js,jsx,ts,tsx}", "components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        strongCyan: "hsl(172,67%,45%)",
+        veryDarkCyan: "hsl(186,14%,43%)",
+        grayishCyan: "hsl(184, 14%, 56%)",
+        lightGrayishCyan: "hsl(185, 41%, 84%)",
+        veryLightGrayishCyan: "hsl(189, 41%, 97%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+    },
   },
   plugins: [],
 };
