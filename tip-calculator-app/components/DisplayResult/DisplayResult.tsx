cf19afa379ce056d6276f02a935b3b49ce7012bf
aniloli42 <aniloli42@gmail.com>
2022-07-08 08:59:25 +0545

    heading level inc

--- tip-calculator-app/components/DisplayResult/DisplayResult.tsx
+++ tip-calculator-app/components/DisplayResult/DisplayResult.tsx
@@ -7,7 +7,7 @@ interface Props {
 const DisplayResult: FC<Props> = ({ title, price }) => {
   return (
     <div className="grid grid-cols-2 gap-x-8">
-      <h3 className="self-start text-white text-sm">{title ?? "Your Title"}</h3>
+      <h2 className="self-start text-white text-sm">{title ?? "Your Title"}</h2>
       <p className="row-[2/3] col-[1/2] text-xs text-grayishCyan">/ person</p>
       <p className="col-[2/3] row-[1/3] self-center justify-self-end text-grayishCyan text-3xl">
         ${price?.toFixed(2) ?? "0"}
